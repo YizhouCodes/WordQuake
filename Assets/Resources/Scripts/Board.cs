ba915e51d245a026b201c1389c99b327e19014a3
YizhouCodes <contact.yizhou@gmail.com>
2018-01-12 14:05:18 +0200

    Added tile-prefab and a basic board
    A basic board implemented with tiles

---
+++ Assets/Resources/Scripts/Board.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour {
+
+    public List<GameObject> tiles = new List<GameObject>();
+    public int gridWidth;
+    public int gridHeight;
+    public GameObject tilePrefab;
+
+	// Use this for initialization
+	void Start () {
+		for(int y = 0; y < gridHeight; y++) {
+            for(int x = 0; x < gridWidth; x++) {
+                GameObject g = Instantiate(tilePrefab, new Vector3(x, y, 0), Quaternion.identity) as GameObject;
+                g.transform.parent = gameObject.transform;
+                tiles.Add(g);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
