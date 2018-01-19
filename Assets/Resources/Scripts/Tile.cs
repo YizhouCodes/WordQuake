829dbb9097d471a7ff07d2a3139d8033af3874d2
YizhouCodes <contact.yizhou@gmail.com>
2018-01-19 17:18:57 +0200

    Letters now display on tiles.

--- Assets/Resources/Scripts/Tile.cs
+++ Assets/Resources/Scripts/Tile.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour {
 
+    string letters = "abcdefghijklmnopqrstuvwxyz";
+    string letter = "";
+    public GameObject Letter;
     public List<Tile> neighbors = new List<Tile>();
 
 	// Use this for initialization
 	void Start () {
-		
+        CreateTile();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void CreateTile()
+    {
+        letter = letters[Random.Range(0, letters.Length)].ToString();
+        Debug.Log(letter);
+        Sprite sprite = Resources.Load("Letters/" + letter, typeof(Sprite)) as Sprite;
+        Debug.Log(sprite);
+        Letter.GetComponent<SpriteRenderer>().sprite = sprite;
+    }
 
     public void AddNeighbor(Tile g)
     {
